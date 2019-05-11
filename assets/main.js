b174d78fe76368ae5764a3c5b73f52e78d1d3109
alex-sir <alexcarbajal710@gmail.com>
2019-05-10 22:00:04 -0700

    Add pause feature

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,26 @@
 let countdown;
 const timerDisplay = document.querySelector("#timer");
+const play = document.querySelector(".fa-play");
+const pause = document.querySelector(".fa-pause");
+const stop = document.querySelector(".fa-stop");
 
 function timer(seconds) {
-    const now = Date.now();
-    const then = now + seconds * 1000;
-    displayTimeLeft(seconds)
+    play.addEventListener("click", () => {
+        clearInterval(countdown);
+        play.style.pointerEvents = "none";
+        const now = Date.now();
+        const then = now + seconds * 1000;
+        displayTimeLeft(seconds);
 
-    countdown = setInterval(() => {
-        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        countdown = setInterval(() => {
+            const secondsLeft = Math.round((then - Date.now()) / 1000);
 
-        if (secondsLeft < 1) {
-            clearInterval(countdown);
-        }
-        displayTimeLeft(secondsLeft);
-    }, 1000);
+            if (secondsLeft < 1) {
+                clearInterval(countdown);
+            }
+            displayTimeLeft(secondsLeft);
+        }, 1000);
+    });
 }
 
 function displayTimeLeft(seconds) {
@@ -24,3 +31,21 @@ function displayTimeLeft(seconds) {
     document.title = `(${display}) Pomodoro`;
     timerDisplay.textContent = display;
 }
+
+function pauseTimer(pause) {
+    pause.addEventListener("click", () => {
+        const time = timerDisplay.textContent.split(":");
+        const seconds = (parseInt(time[0]) * 60) + parseInt(time[1]);
+
+        clearInterval(countdown);
+        timer(seconds);
+        play.style.pointerEvents = "auto";
+    });
+}
+
+function main() {
+    timer(1500);
+    pauseTimer(pause);
+}
+
+window.onload = main();
